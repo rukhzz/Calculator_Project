c9d17e883e1f67d0b12a4ad27c731d5541b04df6
Rukhsana N <rukhsana8943@gmail.com>
2023-11-01 21:31:52 +0530

    add display field

--- FirstWindow.java
+++ FirstWindow.java
@@ -1,4 +1,9 @@
+import java.awt.Color;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.text.AttributeSet.ColorAttribute;
 
 public class FirstWindow {
 
@@ -6,6 +11,18 @@ public class FirstWindow {
         JFrame jf=new JFrame("Calculator");
         jf.setLayout(null);
         jf.setSize(600,600);
+        jf.setLocation(300,150);
+       
+
+        /*label */
+        JLabel displaylabel=new JLabel("put your text");
+        displaylabel.setBounds(30, 50, 540, 40);
+        displaylabel.setBackground(Color.CYAN);
+        displaylabel.setOpaque(true);
+        displaylabel.setHorizontalAlignment(SwingConstants.RIGHT);
+        displaylabel.setForeground(Color.BLACK);
+        jf.add(displaylabel);
+        
         jf.setVisible(true);
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }

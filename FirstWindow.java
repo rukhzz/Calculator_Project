789b3fa8db6d583342252fd25d7384097485f116
Rukhsana N <rukhsana8943@gmail.com>
2023-11-03 19:39:12 +0530

    modified (concatination)

--- FirstWindow.java
+++ FirstWindow.java
@@ -143,37 +143,37 @@ public class FirstWindow implements ActionListener{
 
     public void actionPerformed(ActionEvent e){
         if (e.getSource()==sevenButton){
-            displaylabel.setText("7");
+            displaylabel.setText(displaylabel.getText()+"7");
         }else if (e.getSource()==eightButton){
-            displaylabel.setText("8");
+            displaylabel.setText(displaylabel.getText()+"8");
         }else if (e.getSource()==nineButton){
-            displaylabel.setText("9");
+            displaylabel.setText(displaylabel.getText()+"9");
         }else if (e.getSource()==fourButton){
-            displaylabel.setText("4");
+            displaylabel.setText(displaylabel.getText()+"4");
         }else if (e.getSource()==fiveButton){
-            displaylabel.setText("5");
+            displaylabel.setText(displaylabel.getText()+"5");
         }else if (e.getSource()==sixButton){
-            displaylabel.setText("6");
+            displaylabel.setText(displaylabel.getText()+"6");
         }else if (e.getSource()==oneButton){
-            displaylabel.setText("1");
+            displaylabel.setText(displaylabel.getText()+"1");
         }else if (e.getSource()==twoButton){
-            displaylabel.setText("2");
+            displaylabel.setText(displaylabel.getText()+"2");
         }else if (e.getSource()==threeButton){
-            displaylabel.setText("3");
+            displaylabel.setText(displaylabel.getText()+"3");
         }else if(e.getSource()==dotButton){
-            displaylabel.setText(".");
+            displaylabel.setText(displaylabel.getText()+".");
         }else if (e.getSource()==zeroButton){
-            displaylabel.setText("0");
+            displaylabel.setText(displaylabel.getText()+"0");
         }else if (e.getSource()==equalButton){
-            displaylabel.setText("=");
+            displaylabel.setText(displaylabel.getText()+"=");
         }else if (e.getSource()==divButton){
-            displaylabel.setText("/");
+            displaylabel.setText(displaylabel.getText()+"/");
         }else if (e.getSource()==mulButton){
-            displaylabel.setText("x");
+            displaylabel.setText(displaylabel.getText()+"x");
         }else if (e.getSource()==subButton){
-            displaylabel.setText("-");
+            displaylabel.setText(displaylabel.getText()+"-");
         }else if (e.getSource()==plusButton){
-            displaylabel.setText("+");
+            displaylabel.setText(displaylabel.getText()+"+");
         }
     }
 
